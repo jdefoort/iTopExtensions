cbe57bfbe10258f8c0571dbe67698a71b3dcbda4
jdefoort <jelledefoort@gmail.com>
2018-03-26 16:22:25 +0200

    201803261622
    added NL translation file for vehicle, got everything working

---
+++ jd-class-vehicle/nl.dict.jd-class-vehicle.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('NL NL', 'Dutch', 'Nederlands', array(
+	// Dictionary entries go here
+	'Class:Vehicle' => 'Voertuig',
+	'Class:Vehicle+' => 'Voertuigen zoals auto\'s of vrachtwagens',
+	'Class:Vehicle/Attribute:energysource' => 'Energiebron',
+	'Class:Vehicle/Attribute:energysource+' => 'Wat wordt gebruikt om het voertuig aan te drijven',
+	'Class:Vehicle/Attribute:energysource/Value:diesel' => 'diesel',
+	'Class:Vehicle/Attribute:energysource/Value:euro95' => 'euro 95',
+	'Class:Vehicle/Attribute:energysource/Value:euro98' => 'euro 98',
+	'Class:Vehicle/Attribute:energysource/Value:CNG' => 'CNG',
+	'Class:Vehicle/Attribute:energysource/Value:LPG' => 'LPG',
+	'Class:Vehicle/Attribute:energysource/Value:electrical' => 'elektrisch',
+	'Class:Vehicle/Attribute:energysource/Value:twostroke' => 'tweetakt',
+	
+	
+	'Class:Vehicle/Attribute:licenseplate' => 'Nummerplaat',
+	'Class:Vehicle/Attribute:licenseplate+' => 'Wat is de nummerplaat',
+	'Class:Vehicle/Attribute:chassisnumber' => 'Chassisnummer',
+	'Class:Vehicle/Attribute:chassisnumber+' => 'Wat is het chassisnummer van het voertuig',
+	
+	'Class:Vehicle/Attribute:nextmaintenance' => 'Volgend gepland onderhoud',
+	'Class:Vehicle/Attribute:nextmaintenance+' => 'Wanneer is het volgend gepland onderhoud',
+	'Class:Vehicle/Attribute:nextinspection' => 'Volgende keuring',
+	'Class:Vehicle/Attribute:nextinspection+' => 'Wanneer moet het voertuig nog eens naar de keuring',
+	'Class:Vehicle/Attribute:insuredtill' => 'Verzekering is geldig tot',
+	'Class:Vehicle/Attribute:insuredtill+' => 'Tot wanneer loopt de verzekering op dit voertuig',
+
+));
+
+ 
+
+
+
+?>
