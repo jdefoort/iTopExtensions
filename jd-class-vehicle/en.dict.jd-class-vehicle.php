63d1431414406170614bef47b906470f042eda49
jbostoen <jeffrey.bostoen@izegem.be>
2018-04-09 09:06:11 +0200

    Improved auto-close and vehicle class (#2)
    * Versoin 20180405-1230
    
    jd-autoclose-ticket:
    - added extension.xml
    - changed version numbering
    - looked at the wrong settings (default Combodo AutoClose)
    - fixed $aReport which was set to an empty array at a wrong location
    - made it more efficient to add Ticket types, since it all works the same anyhow right now.
    
    * Version 20180405-1452
    
    - removed dictionary entries for monitor
    - changed them to translations for the current vehicle class
    
    * Version 20180406-1238
    
    - added Typology: Vehicle Driver's License, Vehicle Energy Source
    - added default English translation for vehicle class
    - removed Monitor translation file completely

---
+++ jd-class-vehicle/en.dict.jd-class-vehicle.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('EN US', 'English', 'English', array(
+
+
+	// Typology: Vehicle Driver's License
+	'Class:VehicleDriversLicense' => 'Driver\'s licenses',
+	'Class:VehicleDriversLicense+' => 'Types of driver\'s licenses',
+	'Class:VehicleDriversLicense/Attribute:name' => 'Type',
+	'Class:VehicleDriversLicense/Attribute:vehicles_list' => 'Vehicles',
+	'Class:VehicleDriversLicense/Attribute:vehicles_list+' => 'Overview of vehicles requiring this type of driver\'s license',
+	
+	// Typology: Vehicle Energy Source
+	'Class:VehicleEnergySource' => 'Energy sources',
+	'Class:VehicleEnergySource+' => 'Types of energy',
+	'Class:VehicleEnergySource/Attribute:name' => 'Type',
+	'Class:VehicleEnergySource/Attribute:vehicles_list' => 'Vehicles',
+	'Class:VehicleEnergySource/Attribute:vehicles_list+' => 'Overview of vehicles requiring this type of energy source',
+	
+	
+	// Dictionary entries go here
+	'Class:Vehicle' => 'Vehicle',
+	'Class:Vehicle+' => 'Vehicles such as cars and trucks',
+	'Class:Vehicle/Attribute:vehicleenergysource_id' => 'Energy source',
+	'Class:Vehicle/Attribute:vehicleenergysource_id+' => 'Fuel or energy source',
+	
+	'Class:Vehicle/Attribute:vehicledriverslicense_id' => 'Driver\'s license',
+	'Class:Vehicle/Attribute:vehicledriverslicense_id+' => 'Required driver\'s license to operate this vehicle',
+
+	
+	'Class:Vehicle/Attribute:maintenanceplace' => 'Maintenance place',
+	'Class:Vehicle/Attribute:maintenanceplace+' => 'The maintenance place for this vehicle',
+	'Class:Vehicle/Attribute:payload' => 'Payload',
+	'Class:Vehicle/Attribute:payload+' => 'The payload for this vehicle',
+	'Class:Vehicle/Attribute:vehiclepicture' => 'Vehicle picture',
+	'Class:Vehicle/Attribute:vehiclepicture+' => 'Upload a clear picture of the vehicle',
+	
+	'Class:Vehicle/Attribute:licenseplate' => 'License plate',
+	'Class:Vehicle/Attribute:licenseplate+' => 'License plate',
+	'Class:Vehicle/Attribute:chassisnumber' => 'Chassis number',
+	'Class:Vehicle/Attribute:chassisnumber+' => 'Chassis number',
+	
+	'Class:Vehicle/Attribute:nextmaintenance' => 'Next planned maintenance',
+	'Class:Vehicle/Attribute:nextmaintenance+' => 'Date of next planned maintenance',
+	'Class:Vehicle/Attribute:nextinspection' => 'Next planned inspection',
+	'Class:Vehicle/Attribute:nextinspection+' => 'Date of next planned inspection',
+	'Class:Vehicle/Attribute:insuredtill' => 'Insured until',
+	'Class:Vehicle/Attribute:insuredtill+' => 'Date of insurance renewal/expiration',
+
+));
+
+ 
+
+
+
+?>
