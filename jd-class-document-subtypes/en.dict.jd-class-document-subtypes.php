b6412500b27d169bd28489fd5b833e862cdbd7d9
jdefoort <jelledefoort@gmail.com>
2018-04-20 10:06:01 +0200

    201804201004
    Reworked the jb-poc-class-document-subtypes extension from my colleague jbostoen ( https://github.com/jbostoen/iTop-custom-extensions ) to match our environment needs. Added translationfiles and our own instances (Recreatex and Technical Services)

---
+++ jd-class-document-subtypes/en.dict.jd-class-document-subtypes.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('EN US', 'English', 'English', array(
+
+    //    'Class:SomeClass/Attribute:some_attribute/Value:some_value' => 'your translation',
+    //    'Class:SomeClass/Attribute:some_attribute/Value:some_value+' => 'your translation for a tooltip',
+	'Class:DocumentFileRcx' => 'Document File Recreatex',
+	'Class:DocumentFileRcx+' => 'All documents for Recreatex instance',
+	'Class:DocumentFileTechnicalServices' => 'Document File Technical Services',
+	'Class:DocumentFileTechnicalServices+' => 'All documents for Technical Services instance',
+	
+	'Menu:NewDocument' => 'New document',
+	'Menu:NewDocument+' => 'Add a new document',
+	
+));
+
+
+
+?>
+
+
+
---
+++ jd-class-document-subtypes/nl.dict.jd-class-document-subtypes.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('NL NL', 'Dutch', 'Nederlands', array(
+    //    'Class:SomeClass/Attribute:some_attribute/Value:some_value' => 'your translation',
+    //    'Class:SomeClass/Attribute:some_attribute/Value:some_value+' => 'your translation for a tooltip',
+	'Class:DocumentFileRcx' => 'Recreatex document',
+	'Class:DocumentFileRcx+' => 'Een nieuw document voor Recreatex aanmaken',
+	'Class:DocumentFileTechnicalServices' => 'Technisch document',
+	'Class:DocumentFileTechnicalServices+' => 'Een nieuw document voor de technische dienst aanmaken',
+	
+	'Menu:NewDocument' => 'Nieuw document',
+	'Menu:NewDocument+' => 'Voeg een nieuw document toe',
+	
+));
+
+
+
+?>
+
+
+
