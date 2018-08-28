ad2c14a97d6e3ddb1141a13fabe35947df5a28b4
jdefoort <jelledefoort@gmail.com>
2018-08-28 11:12:31 +0200

    201808281111
    added the locks class, so HR will be able to see which person has which keys in his possession.
    Furthermore i changed the building request menu order to be more logical.

--- jd-class-locks/en.dict.jd-class-locks.php
+++ jd-class-locks/en.dict.jd-class-locks.php
@@ -7,40 +7,27 @@
  */
 
 Dict::Add('EN US', 'English', 'English', array(
-
+	
+	// Dictionary entries go here
+	'Locks/otherinfo' => 'General information',
+	'Locks/baseinfo' => 'Other information',
 
 	// Typology: LockType
 	'Class:LockType' => 'Type of Lock',
 	'Class:LockType+' => 'What type of Lock is this',
 	'Class:LockType/Attribute:name' => 'Type of Lock',
-	'Class:LockType/Attribute:Locks_list' => 'Locks per type',
-	'Class:LockType/Attribute:Locks_list+' => 'Overview of Locks of this type',
+	'Class:LockType/Attribute:lock_list' => 'Locks per type',
+	'Class:LockType/Attribute:lock_list+' => 'Overview of Locks of this type',
 	
-	
-	// Dictionary entries go here
-	'Locks/otherinfo' => 'General information',
-	'Locks/baseinfo' => 'Other information',
-	'Locks/moreinfo' => 'More information',
-	'Locks/date' => 'Date',
-		
-	'Class:Locks' => 'Locks',
-	'Class:Locks+' => 'Locks such as powerLocks and machinery',
-	'Class:Locks/Attribute:Locktype_id' => 'What type of Lock is this',
-	'Class:Locks/Attribute:Locktype_id+' => 'What type is this, example: jackhammer, powerdrill',
-	
-	'Class:Locks/Attribute:purpose_id' => 'Location/ maintask',
-	'Class:Locks/Attribute:purpose_id+' => 'What is the default location/task of this Lock',
 
-	
-	'Class:Locks/Attribute:buildyear' => 'Building year',
-	'Class:Locks/Attribute:buildyear+' => 'In what year is this Lock built',
-	'Class:Locks/Attribute:intern_id' => 'Intern number',
-	'Class:Locks/Attribute:intern_id+' => 'What is the following number we use in our organisation',
-	'Class:Locks/Attribute:contact_id' => 'Responsible person',
-	'Class:Locks/Attribute:contact_id+' => 'Who is responsible for this Lock',
-	'Class:Locks/Attribute:team_id' => 'Team',
-	'Class:Locks/Attribute:team_id+' => 'Which team is responsible',
 
+	'Class:Lock' => 'Locks',
+	'Class:Lock+' => 'Locks',
+	'Class:Lock/Attribute:locktype_id' => 'What type of Lock is this',
+	'Class:Lock/Attribute:locktype_id+' => 'What type is this, example: brand, badge, key,...',
+	'Class:Lock/Attribute:locksort' => 'What type of Lock is this',
+	'Class:Lock/Attribute:locksort+' => 'Analog (physical) or digital (badge)',
+	
 ));
 
  
--- jd-class-locks/nl.dict.jd-class-locks.php
+++ jd-class-locks/nl.dict.jd-class-locks.php
@@ -7,40 +7,29 @@
  */
 
 Dict::Add('NL NL', 'Dutch', 'Nederlands', array(
-
-
-	// Typology: ToolType
-	'Class:ToolType' => 'Type gereedschap',
-	'Class:ToolType+' => 'Welke soort van gereedschap is dit',
-	'Class:ToolType/Attribute:name' => 'Type machine',
-	'Class:ToolType/Attribute:tools_list' => 'Gereedschap per type',
-	'Class:ToolType/Attribute:tools_list+' => 'Overzicht van alle gereedschap per type',
-	
-	// Typology: Purpose
-	'Class:Purpose' => 'Standplaats / Taak',
-	'Class:Purpose+' => 'Waar of door wie wordt dit gereedschap meestal gebruikt',
-	'Class:Purpose/Attribute:name' => 'Standplaats / Taak',
-	'Class:Purpose/Attribute:tools_list' => 'Gereedschap per standplaats / taak',
-	'Class:Purpose/Attribute:tools_list+' => 'Overzicht van alle gereedschap per standplaats / taak',
-	
 	
 	// Dictionary entries go here
-	'Class:Tools' => 'Gereedschap',
-	'Class:Tools+' => 'Gereedschap zoals boormachines en elektrisch gereedschap',
-	'Class:Tools/Attribute:tooltype_id' => 'Type arbeidsmiddel',
-	'Class:Tools/Attribute:tooltype_id+' => 'Welk type gereedschap is dit',
-	
-	'Class:Tools/Attribute:purpose_id' => 'Standplaats / Taak',
-	'Class:Tools/Attribute:purpose_id+' => 'Wat is de standaard standplaats / taak van dit gereedschap',
+	'Locks/otherinfo' => 'Andere informatie',
+	'Locks/baseinfo' => 'Basis informatie',
 
-	'Class:Tools/Attribute:buildyear' => 'Bouwjaar',
-	'Class:Tools/Attribute:buildyear+' => 'Uit welk jaar komt dit gereedschap',
-	'Class:Tools/Attribute:intern_id' => 'Intern volgnummer',
-	'Class:Tools/Attribute:intern_id+' => 'Wat is het interne volgnummer',
-	'Class:Tools/Attribute:contact_id' => 'Verantwoordelijke',
-	'Class:Tools/Attribute:contact_id+' => 'Wie is verantwoordelijk voor dit gereedschap',
-	'Class:Tools/Attribute:team_id' => 'Dienst',
-	'Class:Tools/Attribute:team_id+' => 'Welke dienst is hiervoor verantwoordelijk',
+
+	// Typology: LockType
+	'Class:LockType' => 'Type sleutel',
+	'Class:LockType+' => 'Welke type sleutel is het',
+	'Class:LockType/Attribute:name' => 'Type sleutel',
+	'Class:LockType/Attribute:lock_list' => 'Sleutel per type',
+	'Class:LockType/Attribute:lock_list+' => 'Overzicht van de sleutels volgens type',
+	
+
+		
+	'Class:Lock' => 'Sleutel',
+	'Class:Lock+' => 'Sleutels zoals fysieke sleutels en badges',
+	'Class:Lock/Attribute:locktype_id' => 'Type sleutel',
+	'Class:Lock/Attribute:locktype_id+' => 'Welke type is dit: voorbeeld badge, fysieke sleutel',
+	'Class:Lock/Attribute:locksort' => 'Sleutelsoort',
+	'Class:Lock/Attribute:locksort+' => 'Analoog (fysiek) of digitaal (badge)',
+	
+
 
 ));
 
