0fcbf8e5bf4e1d8c47665a2b409704689d5a68b6
jdefoort <35953763+jdefoort@users.noreply.github.com>
2019-10-22 13:01:29 +0200

    201910221300
    Started making a new module meant to be able to publish some projects on a website so the council and citizens can see what we are doing about those things.

---
+++ jd-class-projectpublication/jd-projectpublication-mgmt/en.dict.jd-projectpublication-mgmt.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('EN US', 'English', 'English', array(
+	// Dictionary entries go here
+	'ProjectPublication:description' => 'Description',
+	'ProjectPublication:baseinfo' => 'Other information',
+	'ProjectPublication:picture' => 'Image',
+	
+	'Class:ProjectPublication' => 'Project publication',
+	'Class:ProjectPublication+' => 'Add a new project publication.',
+	
+	'Class:ProjectPublication/Attribute:title' => 'Title',
+	'Class:ProjectPublication/Attribute:title+' => 'Title',
+	'Class:ProjectPublication/Attribute:org_id' => 'Organisation',
+	'Class:ProjectPublication/Attribute:org_id+' => 'Which organisation',
+	'Class:ProjectPublication/Attribute:contact_id' => 'Who/Which team',
+	'Class:ProjectPublication/Attribute:contact_id+' => 'Who is using this tool/vehicle',
+	'Class:ProjectPublication/Attribute:date_created' => 'Date created',
+	'Class:ProjectPublication/Attribute:date_created+' => 'When was this project submitted',
+	'Class:ProjectPublication/Attribute:state_published' => 'State published',
+	'Class:ProjectPublication/Attribute:state_published+' => 'State published',
+	
+	'Class:ProjectPublication/Attribute:state_published/Value:not_published' => 'Not published',
+	'Class:ProjectPublication/Attribute:state_published/Value:not_published+' => 'Not published',
+	'Class:ProjectPublication/Attribute:state_published/Value:published' => 'Published',
+	'Class:ProjectPublication/Attribute:state_published/Value:published+' => 'Published',
+	'Class:ProjectPublication/Attribute:state_published/Value:rejected' => 'Rejected',
+	'Class:ProjectPublication/Attribute:state_published/Value:rejected+' => 'Rejected',
+		
+	'Class:ProjectPublication/Attribute:short_description' => 'Short description',
+	'Class:ProjectPublication/Attribute:short_description+' => 'Short description',
+	'Class:ProjectPublication/Attribute:tag' => 'Tags',
+	'Class:ProjectPublication/Attribute:tag+' => 'Tags',
+	'Class:ProjectPublication/Attribute:description' => 'Description',
+	'Class:ProjectPublication/Attribute:description+' => 'Description',
+	'Class:ProjectPublication/Attribute:picture' => 'Image',
+	'Class:ProjectPublication/Attribute:picture+' => 'Image that fits the project',
+	
+	'Menu:ProjectPublicationManagement' => 'Project Publication',
+	'Menu:ProjectPublicationManagement+' => 'Project Publication',
+	'Menu:SearchProjectPublication' => 'Search for project',
+	'Menu:SearchProjectPublication+' => 'Search for project publication',
+	'Menu:NewProjectPublication' => 'New project',
+	'Menu:NewProjectPublication+' => 'Add a new project publication',
+));
+
+ 
+
+
+
+?>
---
+++ jd-class-projectpublication/jd-projectpublication-mgmt/nl.dict.jd-projectpublication-mgmt.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('NL NL', 'Dutch', 'Nederlands', array(
+	// Dictionary entries go here
+	'ProjectPublication:description' => 'Omschrijving',
+	'ProjectPublication:baseinfo' => 'Algemene info',
+	'ProjectPublication:picture' => 'Foto',
+	
+	'Class:ProjectPublication' => 'Project publicatie',
+	'Class:ProjectPublication+' => 'Voeg een nieuwe projectpublicatie toe',
+	
+	'Class:ProjectPublication/Attribute:title' => 'Titel',
+	'Class:ProjectPublication/Attribute:title+' => 'Titel',
+	'Class:ProjectPublication/Attribute:org_id' => 'Organisatie',
+	'Class:ProjectPublication/Attribute:org_id+' => 'Welke organisatie',
+	'Class:ProjectPublication/Attribute:contact_id' => 'Naam persoon',
+	'Class:ProjectPublication/Attribute:contact_id+' => 'Naam persoon',
+	'Class:ProjectPublication/Attribute:date_created' => 'Datum aangemaakt',
+	'Class:ProjectPublication/Attribute:date_created+' => 'Datum aangemaakt',
+	'Class:ProjectPublication/Attribute:state_published' => 'Status',
+	'Class:ProjectPublication/Attribute:state_published+' => 'Status',
+	
+	'Class:ProjectPublication/Attribute:state_published/Value:not_published' => 'Niet gepubliceerd',
+	'Class:ProjectPublication/Attribute:state_published/Value:not_published+' => 'Niet gepubliceerd',
+	'Class:ProjectPublication/Attribute:state_published/Value:published' => 'Gepubliceerd',
+	'Class:ProjectPublication/Attribute:state_published/Value:published+' => 'Gepubliceerd',
+	'Class:ProjectPublication/Attribute:state_published/Value:rejected' => 'Geweigerd',
+	'Class:ProjectPublication/Attribute:state_published/Value:rejected+' => 'Geweigerd',
+	
+	'Class:ProjectPublication/Attribute:short_description' => 'Korte uitleg',
+	'Class:ProjectPublication/Attribute:short_description+' => 'Korte uitleg',
+	'Class:ProjectPublication/Attribute:tag' => 'Tags',
+	'Class:ProjectPublication/Attribute:tag+' => 'Tags',
+	'Class:ProjectPublication/Attribute:description' => 'Uitleg',
+	'Class:ProjectPublication/Attribute:description+' => 'Uitleg',
+	'Class:ProjectPublication/Attribute:picture' => 'Foto',
+	'Class:ProjectPublication/Attribute:picture+' => 'Foto die het project omschrijft',
+
+	'Menu:ProjectPublicationManagement' => 'Projectpublicatie',
+	'Menu:ProjectPublicationManagement+' => 'Projectpublicatie',	
+	'Menu:SearchProjectPublication' => 'Zoek een project',
+	'Menu:SearchProjectPublication+' => 'Zoek in projectpublicaties',
+	'Menu:NewProjectPublication' => 'Nieuw project',
+	'Menu:NewProjectPublication+' => 'Registreer een nieuwe projectpublicatie',
+));
+
+ 
+
+
+
+?>
